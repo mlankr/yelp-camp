864ddcf9543fa458958ebab9f860d069beb993e7
Milan <milan.koirala@stud.th-luebeck.de>
2023-08-15 10:21:40 +0200

    test app

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const reviewRoutes = require('./routes/reviews')
 const userRoutes = require('./routes/users')
 
 // database
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl)
     .then(() => {
         console.log('Database Connected!!!')
